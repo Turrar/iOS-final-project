aa14484a940ba850a909678c768c142b1c1d5152
2 <RAR>
2025-06-02 17:49:15 +0500

    Initial Commit

---
+++ NoteKeeper/NoteKeeperApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoteKeeperApp.swift
+//  NoteKeeper
+//
+//  Created by Macbook Air on 02.06.2025.
+//
+
+import SwiftUI
+
+@main
+struct NoteKeeperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ NoteKeeperTests/NoteKeeperTests.swift
@@ -0,0 +1,17 @@
+//
+//  NoteKeeperTests.swift
+//  NoteKeeperTests
+//
+//  Created by Macbook Air on 02.06.2025.
+//
+
+import Testing
+@testable import NoteKeeper
+
+struct NoteKeeperTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
